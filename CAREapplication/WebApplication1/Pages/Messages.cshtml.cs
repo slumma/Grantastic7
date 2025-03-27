778c77c06b655b201444c475f8fc77f0b62412e8
sam <thesamogden@gmail.com>
2025-03-27 16:19:59 -0400

    login & messages pages done kinda

---
+++ CAREapplication/WebApplication1/Pages/Messages.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CAREapplication.Pages
+{
+    public class MessagesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
