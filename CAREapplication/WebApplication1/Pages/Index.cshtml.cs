5f6c9bba8d4384d284066f9d466466fabf0441a6
sam <thesamogden@gmail.com>
2025-03-30 15:04:48 -0400

    Update Index.cshtml.cs

--- CAREapplication/WebApplication1/Pages/Index.cshtml.cs
+++ CAREapplication/WebApplication1/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using lab484.Pages.DB;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -26,6 +27,61 @@ public class IndexModel : PageModel
             HttpContext.Session.Remove("LoginError"); // Clear after displaying
         }
 
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        string loginQuery = "SELECT COUNT(*) FROM users WHERE Username = @Username AND Password = @Password";
+        string findUserID = "SELECT UserID FROM users WHERE Username = @Username AND Password = @Password";
+
+        // check if login credentials are valid
+        if (DBClass.HashedLogin(Username, Password))
+        {
+            DBClass.DBConnection.Close();
+            HttpContext.Session.SetInt32("loggedIn", 1);
+            HttpContext.Session.SetString("username", Username);
+
+            // retrieve userIDs
+            int userID = DBClass.HashedUserID(Username);
+            HttpContext.Session.SetInt32("userID", userID);
+            DBClass.DBConnection.Close();
+
+            // check user permissions
+            int adminStatus = DBClass.adminCheck(userID);
+            DBClass.DBConnection.Close();
+            if (adminStatus == 1)
+            {
+                HttpContext.Session.SetInt32("adminStatus", 1);
+                return RedirectToPage("/Admin/AdminLanding");
+            }
+            else
+            {
+                int facultyStatus = DBClass.facultyCheck(userID);
+                if (facultyStatus == 1)
+                {
+                    DBClass.DBConnection.Close();
+                    HttpContext.Session.SetInt32("facultyStatus", 1);
+                    return RedirectToPage("/Faculty/FacultyLanding");
+                }
+                else
+                {
+                    DBClass.DBConnection.Close();
+                    return RedirectToPage("/NoPermissions");
+                }
+            }
+        }
+        else
+        {
+            ViewData["LoginMessage"] = "Username and/or Password Incorrect";
+            return Page();
+        }
+    }
+
+
+    public IActionResult OnPostLogoutHandler()
+    {
+        HttpContext.Session.Clear();
         return Page();
     }
 }
