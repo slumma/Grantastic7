5613de4f189b8e77d8d9a72cd03533569cab0dbd
sam <thesamogden@gmail.com>
2025-03-30 11:41:26 -0400

    detailed project
    done

---
+++ CAREapplication/WebApplication1/Pages/Project/DetailedProject.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CAREapplication.Pages.Project
+{
+    public class DetailedProjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- CAREapplication/WebApplication1/Pages/Project/ProjectDashboard.cshtml.cs
+++ CAREapplication/WebApplication1/Pages/Project/ProjectDashboard.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CAREapplication.Pages
+namespace CAREapplication.Pages.Project
 {
     public class ProjectDashboardModel : PageModel
     {
