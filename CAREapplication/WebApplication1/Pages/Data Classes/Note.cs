26394314217aaf4f1a2221b9ffba048ea1407b74
white5jd <white5jd@dukes.jmu.edu>
2025-03-30 14:43:21 -0400

    dataclasses

---
+++ CAREapplication/WebApplication1/Pages/Data Classes/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace CAREapplication.Pages.Data_Classes
+{
+    public class Note
+    {
+        public int NoteID { get; set; }
+
+        public int ProjectID { get; set; }
+
+        public String? NoteContents { get; set; }
+
+        public DateTime DatePosted { get; set; }
+    }
+}
--- CAREapplication/WebApplication1/Pages/Data Classes/ProjectStatus.cs
+++ CAREapplication/WebApplication1/Pages/Data Classes/ProjectStatus.cs
@@ -1,13 +1,14 @@
 ﻿namespace CAREapplication.Pages.Data_Classes
 {
-    public class GrantStatus
+    public class ProjectStatus
     {
-        public int GrantStatusID { get; set; }
+        public int StatusId { get; set; }
 
-        public int GrantID { get; set; }
+        public int ProjectID { get; set; }
 
         public String? StatusName { get; set; }
 
         public DateTime ChangeDate { get; set; }
+
     }
 }
