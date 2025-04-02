cb51b84b36f0baef0308817a10f033b433f261d6
white5jd <white5jd@dukes.jmu.edu>
2025-04-02 14:44:12 -0400

    add project

--- CAREapplication/WebApplication1/Pages/Project/AddProject.cshtml.cs
+++ CAREapplication/WebApplication1/Pages/Project/AddProject.cshtml.cs
@@ -5,8 +5,22 @@ namespace CAREapplication.Pages.Project
 {
     public class AddProjectModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            if (HttpContext.Session.GetInt32("loggedIn") != 1)
+            {
+                HttpContext.Session.SetString("LoginError", "You must login to access that page!");
+                return RedirectToPage("../Index"); // Redirect to login page
+            }
+            else if (HttpContext.Session.GetInt32("facultyStatus") != 1 && HttpContext.Session.GetInt32("adminStatus") != 1)
+            {
+                HttpContext.Session.SetString("LoginError", "You do not have permission to access that page!");
+                return RedirectToPage("../Index"); // Redirect to login page
+            }
+            // made a method at the bottom of the file so i dont have to copy and paste it a bunch of times 
+           
+
+            return Page();
         }
     }
 }
