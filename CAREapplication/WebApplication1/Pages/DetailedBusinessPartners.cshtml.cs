06d176d5672c7348a00f16c3495c8a2309427981
sam <thesamogden@gmail.com>
2025-04-06 18:31:16 -0400

    detailed bp need to link to db tho

---
+++ CAREapplication/WebApplication1/Pages/DetailedBusinessPartners.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CAREapplication.Pages
+{
+    public class DetailedBusinessPartnersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
